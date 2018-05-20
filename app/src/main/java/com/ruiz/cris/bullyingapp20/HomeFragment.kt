aabe4f2e27058e229fe3e63620f3686f9c17cfd3
Cris <crisuvas@gmail.com>
2018-05-20 18:35:07 -0500

    working version

---
+++ app/src/main/java/com/ruiz/cris/bullyingapp20/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.ruiz.cris.bullyingapp20
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+
+class HomeFragment : Fragment(){
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view : View = inflater.inflate(R.layout.fragment_home, container, false)
+        recyclerView = view.findViewById(R.id.rv_main)
+        val infographics = ArrayList<Infographic>()
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+
+        infographics.add(Infographic(R.string.titleMainOne, R.string.textMainOne, R.drawable.bullying_main_one))
+        infographics.add(Infographic(R.string.titleMainTwo, R.string.textMainTwo, R.drawable.bullying_main_two))
+        infographics.add(Infographic(R.string.titleMainThree, R.string.textMainThree, R.drawable.bullying_main_three))
+        infographics.add(Infographic(R.string.titleMainFour, R.string.textMainFour, R.drawable.bullying_main_four))
+
+        val adapter = AdapterInfographic(infographics)
+
+        recyclerView.adapter = adapter
+        return view
+    }
+
+}
--- app/src/main/java/com/ruiz/cris/bullyingapp20/MainActivity.kt
+++ app/src/main/java/com/ruiz/cris/bullyingapp20/MainActivity.kt
@@ -1,29 +1,61 @@
 package com.ruiz.cris.bullyingapp20
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.MenuItem
+import android.view.View
 import android.widget.LinearLayout
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var bottonNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val reciclerView : RecyclerView = findViewById(R.id.rv_main)
-        reciclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        val infographics = ArrayList<Infographic>()
 
-        infographics.add(Infographic(R.string.titleMainOne, R.string.textMainOne,R.drawable.bullying_main_one))
-        infographics.add(Infographic(R.string.titleMainTwo, R.string.textMainTwo,R.drawable.bullying_main_two))
-        infographics.add(Infographic(R.string.titleMainThree, R.string.textMainThree,R.drawable.bullying_main_three))
-        infographics.add(Infographic(R.string.titleMainFour, R.string.textMainFour,R.drawable.bullying_main_four))
+        bottonNav = findViewById(R.id.bottom_navigation)
+        bottonNav.setOnNavigationItemSelectedListener(this)
 
-        val adapter = AdapterInfographic(infographics)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
 
-        reciclerView.adapter = adapter
+            /*val reciclerView: RecyclerView = findViewById(R.id.rv_main)
+            reciclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+            val infographics = ArrayList<Infographic>()
+
+            infographics.add(Infographic(R.string.titleMainOne, R.string.textMainOne, R.drawable.bullying_main_one))
+            infographics.add(Infographic(R.string.titleMainTwo, R.string.textMainTwo, R.drawable.bullying_main_two))
+            infographics.add(Infographic(R.string.titleMainThree, R.string.textMainThree, R.drawable.bullying_main_three))
+            infographics.add(Infographic(R.string.titleMainFour, R.string.textMainFour, R.drawable.bullying_main_four))
+
+            val adapter = AdapterInfographic(infographics)
+
+            reciclerView.adapter = adapter*/
 
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var selectedFragment : Fragment? = null
+
+        when(item.itemId){
+            R.id.home_btn ->
+                    selectedFragment = HomeFragment()
+            R.id.quiz_btn ->
+                    selectedFragment = QuizFragment()
+            R.id.stories_btn ->
+                    selectedFragment = StoriesFragment()
+        }
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+
+        return true
+    }
+
 }
