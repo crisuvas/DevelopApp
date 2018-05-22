d0a36e29df5cca778bc6ca53c19aeb67dce59e5f
Cris <crisuvas@gmail.com>
2018-05-21 19:33:05 -0500

    working version

--- app/src/main/java/com/ruiz/cris/bullyingapp20/MainActivity.kt
+++ app/src/main/java/com/ruiz/cris/bullyingapp20/MainActivity.kt
@@ -26,19 +26,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
 
-            /*val reciclerView: RecyclerView = findViewById(R.id.rv_main)
-            reciclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-            val infographics = ArrayList<Infographic>()
-
-            infographics.add(Infographic(R.string.titleMainOne, R.string.textMainOne, R.drawable.bullying_main_one))
-            infographics.add(Infographic(R.string.titleMainTwo, R.string.textMainTwo, R.drawable.bullying_main_two))
-            infographics.add(Infographic(R.string.titleMainThree, R.string.textMainThree, R.drawable.bullying_main_three))
-            infographics.add(Infographic(R.string.titleMainFour, R.string.textMainFour, R.drawable.bullying_main_four))
-
-            val adapter = AdapterInfographic(infographics)
-
-            reciclerView.adapter = adapter*/
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/ruiz/cris/bullyingapp20/QuizFragment.kt
+++ app/src/main/java/com/ruiz/cris/bullyingapp20/QuizFragment.kt
@@ -1,15 +1,28 @@
 package com.ruiz.cris.bullyingapp20
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class QuizFragment : Fragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+
+        val viewQuiz = inflater.inflate(R.layout.fragment_quiz, container, false)
+
+        var buttonQuiz : Button = viewQuiz.findViewById(R.id.enter_btn)
+
+        buttonQuiz.setOnClickListener({
+            val intent = Intent(context, QuizActivity::class.java)
+            startActivity(intent)
+        })
+        return viewQuiz
     }
+
 }
